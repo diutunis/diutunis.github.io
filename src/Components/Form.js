b3d9e12e5b956d9160ec852d273ab7b50598c3d2
zeke <ezekielpaints@gmail.com>
2022-05-17 17:21:08 -0400

    duplicatd the search bar funny glitch

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,39 +1,19 @@
 
 import React from "react";
 import { useEffect, useState } from "react";
-
+import PoemList from "./PoemList"
+import handleSubmit from "./PoemList"
 
 function Form() {
 
     const [searchTerm, setSearchTerm] = useState("");
-    const [poemList, setPoemList] = useState(null);
+  
     
   
     const updateSearchTerm = (event) => {
       setSearchTerm(event.target.value);
     };
-    const handleSubmit = (event) => {
-      // prevent page reload
-      event.preventDefault();
-      fetch(`https://poetrydb.org//lines/${searchTerm}`)
-        .then((response) => response.json())
-        .then((data) => {
-          // reset searchTerm to empty
-          setSearchTerm("");
-          setPoemList(data[0].lines + data[1].lines);
-        });
-    };
   
-    
-    let poemDisplay = "";
-    if (poemList !== null) {
-      poemDisplay = (
-        <div>
-          <h2> {searchTerm}</h2>
-              <p>{poemList}</p>
-        </div>
-      );
-    }
   
     return (
       <div >
@@ -43,8 +23,9 @@ function Form() {
           <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="ammo"/>
           <input type="submit" value="trigger" />
         </form>
+
       
-        {poemDisplay}
+     <PoemList/>
       </div>
   
     );
--- src/Components/PoemList.js
+++ src/Components/PoemList.js
@@ -1,7 +1,48 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import searchTerm from "./Form"
+import setSearchTerm from "./Form"
+import updateSearchTerm from "./Form"
 
 const PoemList =()=>{
+    const [poemList, setPoemList] = useState(null);
+
+    const handleSubmit = (event) => {
+        // prevent page reload
+        event.preventDefault();
+        fetch(`https://poetrydb.org//lines/${searchTerm}`)
+          .then((response) => response.json())
+          .then((data) => {
+            // reset searchTerm to empty
+            setSearchTerm("");
+            setPoemList(data[0].lines + data[1].lines);
+          });
+      };
+    
+      
+      let poemDisplay = "";
+      if (poemList !== null) {
+        poemDisplay = (
+          <div>
+            <h2> {searchTerm}</h2>
+                <p>{poemList}</p>
+          </div>
+        );
+      }
+    
+      return (
+        <div >
+          <h1>︻╦╤─</h1>
+          <form onSubmit={handleSubmit}>
+            <label>reload:</label>
+            <input onChange={updateSearchTerm} value={searchTerm} type="text" placeholder="ammo"/>
+            <input type="submit" value="trigger" />
+          </form>
+        
+          {poemDisplay}
+        </div>
+    
+      );
 
 }
 
